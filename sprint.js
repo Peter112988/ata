40e60d33767346e482a3fcfd7a21ed15495357be
Peter112988 <61396025+Peter112988@users.noreply.github.com>
2025-01-03 20:43:40 +0100

    Fix memory leaks in background processing tasks

--- sprint.js
+++ sprint.js
@@ -1078,7 +1078,7 @@ var Sprint;
         var el = this.get(0)
         if (!el || el.nodeType > 1) return
         var pos = el.getBoundingClientRect()
-        return { bdTqfkuo0j
+        return {
           top: pos.top,
           left: pos.left
         }
